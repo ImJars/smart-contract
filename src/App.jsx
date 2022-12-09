4a45ec9034127388545dd4fc70a2dcc76c14e6d8
ImJars <ra374885@uaeh.edu.mx>
2022-12-08 22:42:02 -0600

    Example

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
       >
         <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
         <div
-          className='absolute top-1/2 left-1/4 flex flex-col justify-start'
+          className='w-auto absolute top-1/4 transform translate-x-1/4 translate-y-full 
+                     justify-start'
         >
           <h1 className='text-base text-white '>
             Hi everyone!
@@ -20,6 +21,11 @@ function App() {
           <h3 className='text-4xl text-secondary'>
             I build things for the web.
           </h3>
+          <h4 className='text-sm text-white w-2/3'>
+            I'm a software developer specializing in creating exceptional 
+            products and digital experiences. I am currently focused on 
+            creating <font className='text-secondary'>Web3</font> technology products.
+          </h4>
         </div>
       </div>
     </>
