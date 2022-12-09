424d222b90b8157b630516366eb3bbf7c69860fb
ImJars <ra374885@uaeh.edu.mx>
2022-12-08 22:31:34 -0600

    Add Spline

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
     <>
-      <h1
-        className='text-4xl font-bold text-center text-gray-900'
+      <div
+        className='w-full h-screen relative'
       >
-        Hola Mundo
-      </h1>
+        <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+        <div
+          className='absolute top-1/2 left-1/4 flex flex-col justify-start'
+        >
+          <h1 className='text-base text-white '>
+            Hi everyone!
+          </h1>
+          <h2 className='text-6xl text-white font-bold'>
+            I'm Angel Ramirez
+          </h2>
+          <h3 className='text-4xl text-secondary'>
+            I build things for the web.
+          </h3>
+        </div>
+      </div>
     </>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'secondary': '#7B90FB',
+        'tertiary': '#F21E4E',
+      },
+    },
   },
   plugins: [],
 }
